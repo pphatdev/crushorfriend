a1d64592c065d21fb488738d5d5873d1a2b1d3b6
PPhat Dev <pphat.kits@gmail.com>
2026-04-18 02:43:40 +0000

    Create crush or friend  in Python

---
+++ crushorfriend.py
@@ -0,0 +1,11 @@
+def greet(input_str):
+    message = "You have a friend!"
+
+    if input_str == "crush":
+        message = "You have a crush!"
+
+    print(message)
+
+
+# crush, friend
+greet("crush")
