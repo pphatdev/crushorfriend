4088732707a9fedd55e80dba66c8101ce99ff74b
PPhat Dev <pphat.kits@gmail.com>
2026-04-18 09:08:58 +0700

    Crush or Friend in TypeScript
    const inputStr: string = "crush"; // crush, friend
    let message: string = "You have a friend!";
    
    if (inputStr === "crush") {
        message = "You have a crush!";
    }
    
    console.log(message);

---
+++ crushorfriend.ts
@@ -0,0 +1,8 @@
+const inputStr: string = "crush"; // crush, friend
+let message: string = "You have a friend!";
+
+if (inputStr === "crush") {
+    message = "You have a crush!";
+}
+
+console.log(message);
